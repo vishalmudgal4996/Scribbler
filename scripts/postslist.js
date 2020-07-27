8e00d44c2d721cb9ff457af01eabeb1805dbd31e
vishalmudgal4996 <vishalmudgal4996@gmail.com>
2020-07-27 21:21:43 +0530

    delete-post modal added to all cards

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -1,5 +1,10 @@
 // to add event listener to all delete button
+for (var i = 0; i < 5; i++) {
+  document
+    .getElementsByClassName("sub-item-2")
+    [i].setAttribute("data-toggle", "modal");
 
-for(int i=0;i<5;i++){
-    
+  document
+    .getElementsByClassName("sub-item-2")
+    [i].setAttribute("data-target", "#delete-post");
 }
