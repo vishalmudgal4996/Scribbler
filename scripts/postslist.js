5759d58704fdd80da7d6240b7c2d01a9ef5e8551
vishalmudgal4996 <vishalmudgal4996@gmail.com>
2020-07-27 22:21:33 +0530

    partB implemented

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -8,3 +8,38 @@ for (var i = 0; i < 5; i++) {
     .getElementsByClassName("sub-item-2")
     [i].setAttribute("data-target", "#delete-post");
 }
+
+function noDeleteModalBtn() {
+  var url = window.location.href;
+  window.location.href = url;
+}
+
+//giving id to each post-card
+
+for (var i = 0; i < 5; i++) {
+  document
+    .getElementsByClassName("post-card")
+    [i].setAttribute("id", "card-" + i);
+}
+
+//adding every posts in a array
+var postCardList = [];
+
+for (var i = 0; i < 5; i++) {
+  var card = document.getElementById("card-" + i);
+  postCardList.push(card);
+}
+
+//functionality not working properly...toDo
+function yesDeleteModalBtn() {
+  document.getElementById("card-0").style.display = "none";
+}
+
+//navigate to post.html file
+for (var i = 0; i < 5; i++) {
+  document
+    .getElementsByClassName("sub-item-3")
+    [i].addEventListener("click", function () {
+      window.location.href = "post.html";
+    });
+}
