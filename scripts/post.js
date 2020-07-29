eef8ca65cd93ce133811e5b699852502a0ecd851
vishalmudgal4996 <vishalmudgal4996@gmail.com>
2020-07-29 21:51:50 +0530

    clean coding done for post....post head and msg added

--- scripts/post.js
+++ scripts/post.js
@@ -1,85 +1,29 @@
-function editComment() {}
-function incrementCounter() {
-  var cntr = document.getElementById("counterLikedBy");
-  var val = parseInt(cntr.innerText);
-  cntr.innerText = val + 1;
-}
-
+//making post button toggle
 function toggleEditSave() {
-  var toggleSave =
-    '<button class="postEdit">Save ' +
-    '<i style="font-size: 14px;font-weight: bolder;" ' +
-    'class="fa fa-save" aria-hidden="true"></i>' +
-    "</button>";
-  var toggleEdit =
-    '<button class="postEdit">Edit ' +
-    '<i style="font-size: 14px;font-weight: bolder;" ' +
-    'class="fa fa-pencil-square-o" aria-hidden="true"></i>' +
-    "</button>";
+  var toggleEdit = document.getElementById("editPostSection");
 
-  var edtSaveBtn = document.getElementById("editPost");
-  var toDo = edtSaveBtn.innerText.toString().trim();
+  if (toggleEdit != null) {
+    if (toggleEdit.style.display === "none") {
+    } else {
+      var savePostbtn =
+        "<div id='savePostSection'><button id='savePost' class='post-btn'>Save <i class='fa fa-save'></i></button></div> ";
+      document.getElementsByClassName("postButtons")[0].innerHTML = savePostbtn;
 
-  if (toDo.localeCompare("Edit") === 0) {
-    // console.log("Currently Edit");
-    var bSave = document.getElementById("editPost");
-    bSave.innerHTML = toggleSave;
-    enableEditableText();
+      var element = document.getElementById("post-head");
+      element.classList.add("editable-border");
+
+      var element = document.getElementById("post-msg");
+      element.classList.add("editable-border");
+    }
   } else {
-    // console.log("Currently Save");
-    var bEdit = document.getElementById("editPost");
-    bEdit.innerHTML = toggleEdit;
-    updateEditedBlog();
+    var editPostbtn =
+      "<div id='editPostSection'><button id='editPost' class='post-btn'>Edit <i class='fa fa-pencil-square-o'></i></button></div>";
+    document.getElementsByClassName("postButtons")[0].innerHTML = editPostbtn;
+
+    var element = document.getElementById("post-head");
+    element.classList.remove("editable-border");
+
+    var element = document.getElementById("post-msg");
+    element.classList.remove("editable-border");
   }
-}
-
-function enableEditableText() {
-  var getBlogContentEle = document.getElementById("blogBody");
-  var getBlogContent = getBlogContentEle.innerText;
-
-  //Div element
-  var getDiv = document.getElementById("blogBodyId");
-  //Create a input element
-  var inputElement =
-    '<textarea class="txtBlog" id="txtEditedBlog" rows="10" cols="205">' +
-    getBlogContent +
-    "</textarea>";
-  //Update the Div element
-  getDiv.innerHTML = inputElement;
-}
-function updateEditedBlog() {
-  var editedBlog = document.getElementById("txtEditedBlog");
-
-  var textEdited = editedBlog.innerHTML.toString();
-
-  var pElement = '<p id="blogBody">' + textEdited + "</p>";
-  var getDiv = document.getElementById("blogBodyId");
-  getDiv.innerHTML = pElement;
-
-  var toggleTitileSave =
-    "UPDATED: <h id='blogTitleNew'>Everything you should know about 'module' & 'require' in Node.js</h>";
-
-  var bEditUpdate = document.getElementById("blogTitleNew");
-  bEditUpdate.innerHTML = toggleTitileSave;
-}
-function addComment() {
-  var x = document
-    .getElementById("txtCommentCommentPost")
-    .addEventListener("onchange", function () {
-      document.getElementById("txtCommentCommentPost").innerHTML;
-    });
-
-  var x = document.getElementById("txtCommentCommentPost").value;
-
-  var pElement = "<p >" + x + "</p>";
-  var getDiv = document.getElementById("listAllComments");
-  //   getDiv.innerHTML = pElement + getDiv.innerHTML;
-}
-
-function addComments(id) {
-  var addEventName = id.value + "<br>";
-  var a = document.getElementById("userComments");
-  document.getElementById("addEventNames").innerHTML +=
-    "<p class='comment-para'>" + addEventName + "</p>";
-  a.value = a.defaultValue;
 }
